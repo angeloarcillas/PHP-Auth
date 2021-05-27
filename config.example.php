5875a590a9747ad273401d6d444b43d5460180d0
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-05-27 16:38:23 +0800

    Create config.example.php file

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'database' => [
+        'name' => 'DATABASE_NAME',
+        'username' => 'DATABASE_USERNAME',
+        'password' => 'DATABASE_PASSWORD',
+        'connection' => 'DATABASE_CONNECTION',
+        'options' => [
+            'CONNECTION_OPTIONS'
+        ],
+    ],
+];
